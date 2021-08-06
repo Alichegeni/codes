a4cf38f94d949c05033c0c88d4fc591ba3c274ec
mrnahich <58499504+mrnahich@users.noreply.github.com>
2021-08-06 18:18:47 +0430

    Add files via upload

---
+++ time2sec.py
@@ -0,0 +1,7 @@
+import time
+hour = int(input("type your hour = "))
+minute = int(input("type your minute = "))
+second = int(input("type your second = "))
+h = (hour * 3600)
+m = (minute * 60)
+print ( h + m + second )
