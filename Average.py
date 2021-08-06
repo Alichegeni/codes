3d7d946caaf2e49d5fc1aed0a29ed60f02f42d78
mrnahich <58499504+mrnahich@users.noreply.github.com>
2021-08-06 13:40:23 +0430

    Delete Average.py

--- Average.py
+++
@@ -1,9 +0,0 @@
-n = input("please write your full name: ")
-a = float(input("please enter your first course point: "))
-b = float(input("please enter your second course point: "))
-c = float(input("please enter your third course point: "))
-
-average = (a + b + c) / 3
-   
-print("your avarage is: ", average)
-
